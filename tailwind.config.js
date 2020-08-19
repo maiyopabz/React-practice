503d5c9ecc2bb280c7af145f8983154efc53fcc1
Maiyo <maiyopabz@gmail.com>
2020-08-18 21:39:35 -0700

    added setShowmenu and onClick

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroudColor: {
+        'black-t-50': 'rgba(0,0,0,0.5',
+      }
+    },
   },
   variants: {},
   plugins: [],
